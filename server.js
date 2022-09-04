4bc5e067abaa0125ab621e9c7fb64874fbd5af6e
mavaldot <48870467+mavaldot@users.noreply.github.com>
2022-09-04 16:48:33 -0500

    fix test hanging issue

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ app.get('/hola', (req, res) => {
     res.send({msg: 'hola'});
 });
 
-app.get('/hello', (req, res) => {
-    res.send({msg: 'hello'});
-});
-
 app.listen(PORT, () => {
     console.log(`Started on port: ${PORT}`);
 });
